cb9211f3f9ae321f49973149d45271bf5528116d
thatsarjun <59558718+thatsarjun@users.noreply.github.com>
2022-06-14 14:32:28 +0530

    added netcore service worker file

---
+++ 55288-5351-sw.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: 'AIzaSyC3iJAhQEoY7-efeg4a9ESDj2-dm3zIncs',
+    messagingSenderId: '717176733832',
+    appId: '1:717176733832:web:b2d996836ba7c006199f3d',
+    projectId: 'github-thatsme',
+    user_key: 'ADGMOT35CHFLVDHBJNIG50K96BIDTAJLJ4MTJ23F70P6A9LDU370',
+    siteid: '7dad5b57968676dab895b3f3b902a0e8'
+};
+importScripts('//cdnt.netcoresmartech.com/swv4.js');
