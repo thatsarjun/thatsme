91989e77b5995ee7a2bd6229991c3fbd25210625
thatsarjun <59558718+thatsarjun@users.noreply.github.com>
2022-06-20 10:41:32 +0530

    added new sw.js

---
+++ 22206-5369-sw.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: 'AIzaSyC3iJAhQEoY7-efeg4a9ESDj2-dm3zIncs',
+    messagingSenderId: '717176733832',
+    appId: '1:717176733832:web:b2d996836ba7c006199f3d',
+    projectId: 'github-thatsme',
+    user_key: 'ADGMOT35CHFLVDHBJNIG50K969312999ETB56ADUIR5BHOAC7VIG',
+    siteid: '4f834ac90b1e1302a3d24efc16d2dd79'
+};
+importScripts('//cdnt.netcoresmartech.com/swv4.js');
